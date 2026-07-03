c0813b99df6a31f151b151de4b306a22d99dbcae
Ganjar Manggala <ganjar@backbase.com>
2026-07-03 17:57:13 +0200

    first commit project playwright

---
+++ tests/landingpage.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://store.steampowered.com/');
+  await expect(page).toHaveTitle("Welcome to Steam");
+  await expect(page).toHaveURL('https://store.steampowered.com/');
+});
