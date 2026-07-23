c629654f691313eeb078962204d48587bd5b001a
Abdulrozzaq <amusarosaq27@gmail.com>
2026-07-23 15:29:08 +0100

    Add HomePage POM and main navigation tests

---
+++ pages/HomePage.ts
@@ -0,0 +1,66 @@
+import{Page, Locator} from '@playwright/test';
+
+/**
+ * HomePage
+ *
+ * Page Object for the Steam homepage.
+ * Contains reusable locators and actions for the main navigation.
+ */
+
+export class HomePage{
+  readonly page: Page;
+
+  // Main navigation links
+  readonly storeLink : Locator;
+  readonly communityLink : Locator;
+  readonly aboutLink : Locator;
+  readonly supportLink : Locator;
+
+  constructor(page :Page){
+    this.page = page;
+
+    this.storeLink= page.getByRole('link',{name: /store/i});
+    this.communityLink = page.getByRole('link',{name: /community/i});
+    this.aboutLink = page.getByRole('link',{name: /about/i});
+    this.supportLink = page.getByRole('link',{name: /support/i});
+  }
+
+    // Opens the Steam homepage
+
+  async open() {
+    await this.page.goto('https://store.steampowered.com/', {
+      waitUntil : 'domcontentloaded',
+    });
+  }
+
+  async clickStore() {
+    await this.storeLink.click();
+  }
+
+  async clickCommunity() {
+    await this.communityLink.click();
+  }
+
+  async clickAbout() {
+    await this.aboutLink.click();
+  }
+
+  async clickSupport() {
+    await this.supportLink.click();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
---
+++ tests/mainNavigation.spec.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@playwright/test';
+import { HomePage} from '../pages/HomePage';
+
+// Verify Store navigation
+
+test('store navigation', async ({page}) => {
+    const homePage = new HomePage(page)
+    
+    await homePage.open();
+
+    await expect(homePage.storeLink).toBeVisible();
+
+    await expect(page).toHaveURL('https://store.steampowered.com/');
+    await expect(page).toHaveTitle("Welcome to Steam");
+
+
+    
+
+});
+
+
+    // Verify community navigation
+
+test('community navigation', async ({page}) => {
+    const homePage = new HomePage(page)
+
+    await homePage.open();
+
+    await expect (homePage.communityLink).toBeVisible();
+
+    await homePage.clickCommunity();
+
+    await expect(page).toHaveURL('https://steamcommunity.com/')
+    await expect(page).toHaveTitle('Steam Community')
+
+    });
+
+
+
+    // Verify about navigation
+
+    test('about navigation', async ({page}) => {
+
+        const homePage = new HomePage(page)
+
+        await homePage.open();
+
+        await expect(homePage.aboutLink).toBeVisible();
+
+        await homePage.clickAbout();
+
+        await expect(page).toHaveURL('https://store.steampowered.com/about/')
+        await expect(page).toHaveTitle('Steam, The Ultimate Online Game Platform')
+
+
+
+    });
+
+
+    // Verify Support navigation
+
+    test('support navigation', async ({page}) => {
+        const homePage = new HomePage(page)
+
+        await homePage.open();
+        await expect(homePage.supportLink).toBeVisible();
+
+        await homePage.clickSupport();
+
+        await expect(page).toHaveURL('https://help.steampowered.com/en/')
+        await expect(page).toHaveTitle('Steam Support')
+    })
